d4bbd7e66fb8d3165f4d50ea47d86ae15d929144
namoscagnm <martins.yo@gmail.com>
2016-12-31 01:15:49 +0100

    no big deal

--- 0004-execvp-wait-001.c
+++ 0004-execvp-wait-001.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <sys/types.h>
+#include <unistd.h>
 
-#define MAX_COUNT 200
+#define MAX_COUNT 5000
 
 void doChildJob(int);
 void doParentJob(int);
@@ -19,6 +20,8 @@ int main (void) {
   else if (pid==0){
     doChildJob(1);
     printf("Child is now wants to calculate more!\n");
+    execvp("/usr/bin/galculator",NULL);
+    printf("April fools day: This text will never see daylight\n");
   }
   else if (pid>0) {
     printf("Parent:Before you freeze me, let me spea...\n");
@@ -31,7 +34,7 @@ int main (void) {
 void doChildJob(int index){
   printf("***Stage %d, Child turn***\n",index);
   int i;
-  for (i=0;i<=MAX_COUNT;i++){
+  for (i=0;i<=MAX_COUNT/100;i++){
     printf("Stage %d, Child talks: %d\n",index,i);
   }
 }
@@ -39,6 +42,7 @@ void doChildJob(int index){
 void doParentJob(int index){
   printf("***Stage %d, Parent turn***\n",index);
   int i;
+  double makeSlow;
   for (i=0;i<=MAX_COUNT;i++){
     printf("Stage %d, Parent talks: %d\n",index,i);
   }
